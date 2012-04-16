65c8d89788bd9fc24f28deecc6411237dc5492b6
Михаил Костенчук <kmi9.work@gmail.com>
2012-04-16 22:53:21 +0400

    postgres

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 Nadya::Application.initialize!
